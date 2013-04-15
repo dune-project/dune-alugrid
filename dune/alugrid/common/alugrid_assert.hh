cdde1655e864a7dab3f725e45b02a73af2082103
Andreas Dedner <a.s.dedner@warwick.ac.uk>
2013-04-15 21:47:51 +0100

    small warning not define both NDEBUG and ALUGRIDDEBUG - gives many warnings
    Former-commit-id: 87063da3c42e95e0c3d3f90448ed82dbb778e1c9

--- dune/alugrid/common/alugrid_assert.hh
+++ dune/alugrid/common/alugrid_assert.hh
@@ -6,6 +6,9 @@
 #ifndef ALUGRIDDEBUG
 # define alugrid_assert(EX) (__ASSERT_VOID_CAST (0))
 #else
+#ifdef NDEBUG
+#warning defined ALUGRIDDEBUG and NDEBUG both leads to many warnings and is probably not reasonable...
+#endif
 # define alugrid_assert(EX) assert(EX)
 #endif
 
