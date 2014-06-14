37097fe69ba132d825f534f4239c9c92053fed71
dedner <a.s.dedner@warwick.ac.uk>
2014-06-14 11:21:08 +0100

    bug fix
    Former-commit-id: 1705bf4dca7f5194205515b3449496e5b26e056d

--- examples/loadbalance/loadbalance_zoltan.hh
+++ examples/loadbalance/loadbalance_zoltan.hh
@@ -1,4 +1,194 @@
+#ifndef LOADBALANCE_ZOLTAN_HH
+#define LOADBALANCE_ZOLTAN_HH
+
+#include <dune/grid/common/gridenums.hh>
+
 #if HAVE_ZOLTAN 
+
+template< class Grid >
+class ZoltanLoadBalanceHandle
+{
+  typedef ZoltanLoadBalanceHandle This;
+
+private:
+  typedef typename Grid::GlobalIdSet GlobalIdSet;
+  typedef typename GlobalIdSet::IdType GIdType;
+  static const int dimension = Grid :: dimension;
+  static const int NUM_GID_ENTRIES = 1;
+  template< int codim >
+  struct Codim
+  {
+    typedef typename Grid :: Traits :: template Codim< codim > :: Entity Entity;
+    typedef typename Grid :: Traits :: template Codim< codim > :: EntityPointer EntityPointer;
+  };
+
+  struct ZoltanPartitioning{
+    int changes; // 1 if partitioning was changed, 0 otherwise 
+    int numGidEntries;  // Number of integers used for a global ID 
+    int numLidEntries;  // Number of integers used for a global ID 
+    int numExport;      // Number of vertices I must send to other processes
+    int numImport;      // Number of vertices I must send to other processes
+    unsigned int *importLocalGids;  // Global IDs of the vertices I must send 
+    unsigned int *importGlobalGids; // Global IDs of the vertices I must send 
+    unsigned int *exportLocalGids;  // Global IDs of the vertices I must send 
+    unsigned int *exportGlobalGids; // Global IDs of the vertices I must send 
+    int *importProcs;    // Process to which I send each of the vertices 
+    int *exportProcs;    // Process to which I send each of the vertices 
+    int *importToPart;
+    int *exportToPart;
+  };
+  struct FixedElements {
+    int fixed_entities;
+    std::vector<int> fixed_GID;
+    std::vector<int> fixed_Process;
+    FixedElements() : fixed_GID(0), fixed_Process(0) {}
+  };
+  struct HGraphData {              /* Zoltan will partition vertices, while minimizing edge cuts */
+    int numMyVertices;             /* number of vertices that I own initially */
+    ZOLTAN_ID_TYPE *vtxGID;        /* global ID of these vertices */
+    float *vtxWEIGHT;              /* weight for each node in the graph */
+    int numMyHEdges;               /* number of my hyperedges */
+    int numAllNbors;               /* number of vertices in my hyperedges */
+    ZOLTAN_ID_TYPE *edgeGID;       /* global ID of each of my hyperedges */
+    int *nborIndex;                /* index into nborGID array of edge's vertices */
+    ZOLTAN_ID_TYPE *nborGID;       /* Vertices of edge edgeGID[i] begin at nborGID[nborIndex[i]] */
+    float *nborWEIGHT;             /* weight for each graph edge */
+    int *nborPROC;                 /* processor for each edge node */
+    FixedElements fixed_elmts;
+    HGraphData() : vtxGID(0), vtxWEIGHT(0), edgeGID(0), nborIndex(0), nborGID(0), nborWEIGHT(0), nborPROC(0) {}
+    ~HGraphData() { freeMemory();}
+    void freeMemory() 
+    {
+      if (!nborWEIGHT)
+        free(nborWEIGHT);
+      if (!nborPROC)
+        free(nborPROC);
+      if (!nborGID)
+        free(nborGID); 
+      if (!nborIndex)
+        free(nborIndex);
+      if (!edgeGID)
+        free(edgeGID);
+      if (!vtxWEIGHT)
+        free(vtxWEIGHT);
+      if (!vtxGID)
+        free(vtxGID);
+    }
+  };
+public:
+  typedef typename Codim< 0 > :: Entity Element;
+  ZoltanLoadBalanceHandle ( const Grid &grid);
+  ~ZoltanLoadBalanceHandle();
+
+  bool userDefinedPartitioning () const
+  {
+    return true;
+  }
+
+  // returns true if user defined partitioning needs to be readjusted 
+  bool repartition ()
+  { 
+    int elements = grid_.size(0);
+    size_t sumElements = grid_.comm().sum( elements );
+    size_t minElements = grid_.comm().min( elements );
+    size_t maxElements = grid_.comm().max( elements );
+    double mean = sumElements / grid_.comm().size();
+    const bool repartition = ((maxElements > (ldbOver_ * mean)) || (minElements < (ldbUnder_ * mean) )) 
+                             ? true : false ; 
+    if (!repartition) 
+      return false;
+    if (!first_)
+    {
+      Zoltan_LB_Free_Part(&(new_partitioning_.importGlobalGids), 
+                   &(new_partitioning_.importLocalGids), 
+                   &(new_partitioning_.importProcs), 
+                   &(new_partitioning_.importToPart) );
+      Zoltan_LB_Free_Part(&(new_partitioning_.exportGlobalGids), 
+                   &(new_partitioning_.exportLocalGids), 
+                   &(new_partitioning_.exportProcs), 
+                   &(new_partitioning_.exportToPart) );
+    }
+    generateHypergraph();
+    /******************************************************************
+    ** Zoltan can now partition the vertices of hypergraph.
+    ** In this simple example, we assume the number of partitions is
+    ** equal to the number of processes.  Process rank 0 will own
+    ** partition 0, process rank 1 will own partition 1, and so on.
+    ******************************************************************/
+    Zoltan_LB_Partition(zz_, // input (all remaining fields are output)
+          &new_partitioning_.changes,        // 1 if partitioning was changed, 0 otherwise 
+          &new_partitioning_.numGidEntries,  // Number of integers used for a global ID 
+          &new_partitioning_.numLidEntries,  // Number of integers used for a local ID 
+          &new_partitioning_.numImport,      // Number of vertices to be sent to me 
+          &new_partitioning_.importGlobalGids,  // Global IDs of vertices to be sent to me 
+          &new_partitioning_.importLocalGids,   // Local IDs of vertices to be sent to me 
+          &new_partitioning_.importProcs,    // Process rank for source of each incoming vertex 
+          &new_partitioning_.importToPart,   // New partition for each incoming vertex 
+          &new_partitioning_.numExport,      // Number of vertices I must send to other processes
+          &new_partitioning_.exportGlobalGids,  // Global IDs of the vertices I must send 
+          &new_partitioning_.exportLocalGids,   // Local IDs of the vertices I must send 
+          &new_partitioning_.exportProcs,    // Process to which I send each of the vertices 
+          &new_partitioning_.exportToPart);  // Partition to which each vertex will belong 
+    first_ = false;
+    return (new_partitioning_.changes == 1);
+  }
+  
+  // return destination (i.e. rank) where the given element should be moved to 
+  // this needs the methods userDefinedPartitioning to return true
+  int operator()( const Element &element ) const 
+  { 
+	  std::vector<int> elementGID(NUM_GID_ENTRIES);
+    // GIdType id = globalIdSet_.id(element);
+    // id.getKey().extractKey(elementGID);
+	  elementGID[0] = grid_.macroView().macroId(element); //   element.impl().macroID();
+
+    // add one to the GIDs, so that they match the ones from Zoltan
+    transform(elementGID.begin(), elementGID.end(), elementGID.begin(), bind2nd(std::plus<int>(), 1));
+
+    int p = int(grid_.comm().rank());
+
+    for (int i = 0; i<new_partitioning_.numExport; ++i)
+    {
+      if (std::equal(elementGID.begin(),elementGID.end(), &new_partitioning_.exportGlobalGids[i*new_partitioning_.numGidEntries]) )
+      {
+        p = new_partitioning_.exportProcs[i];
+        break;
+      }
+    }
+    return p;
+  }
+private:
+  void generateHypergraph();
+
+  // ZOLTAN query functions
+  static int get_number_of_vertices(void *data, int *ierr);
+  static void get_vertex_list(void *data, int sizeGID, int sizeLID,
+              ZOLTAN_ID_PTR globalID, ZOLTAN_ID_PTR localID,
+              int wgt_dim, float *obj_wgts, int *ierr);
+  static int get_num_fixed_obj(void *data, int *ierr);
+  static void get_fixed_obj_list(void *data, int num_fixed_obj,
+                                 int num_gid_entries, ZOLTAN_ID_PTR fixed_gids, int *fixed_part, int *ierr);
+  static void get_num_edges_list(void *data, int sizeGID, int sizeLID,
+                                 int num_obj,
+                                 ZOLTAN_ID_PTR globalID, ZOLTAN_ID_PTR localID,
+                                 int *numEdges, int *ierr);
+  static void get_edge_list(void *data, int sizeGID, int sizeLID,
+                            int num_obj, ZOLTAN_ID_PTR globalID, ZOLTAN_ID_PTR localID,
+                            int *num_edges,
+                            ZOLTAN_ID_PTR nborGID, int *nborProc,
+                            int wgt_dim, float *ewgts, int *ierr);
+
+  const Grid &grid_;
+  const GlobalIdSet &globalIdSet_;
+
+  Zoltan_Struct *zz_;
+  HGraphData hg_;
+  ZoltanPartitioning new_partitioning_;
+  bool first_;
+  double ldbUnder_, ldbOver_;
+  const bool fix_bnd_;
+};
+
 template< class Grid >
 ZoltanLoadBalanceHandle<Grid>::
 ZoltanLoadBalanceHandle(const Grid &grid)
@@ -13,7 +203,7 @@ ZoltanLoadBalanceHandle(const Grid &grid)
   // General parameters
   Zoltan_Set_Param(zz_, "DEBUG_LEVEL", "0");
   if (!fix_bnd_) // fixing element requires using hypergraph partitioning (which is perhaps better anyway?)
-    Zoltan_Set_Param(zz_, "LB_METHOD", "HYPERGRAPH");        /* partitioning method */
+    Zoltan_Set_Param(zz_, "LB_METHOD", "GRAPH");        /* partitioning method */
   else
     Zoltan_Set_Param(zz_, "LB_METHOD", "HYPERGRAPH");        /* partitioning method */
   Zoltan_Set_Param(zz_, "HYPERGRAPH_PACKAGE", "PHG"); /* version of method */
@@ -31,7 +221,7 @@ ZoltanLoadBalanceHandle(const Grid &grid)
   /* PHG parameters  - see the Zoltan User's Guide for many more
   */
   Zoltan_Set_Param(zz_, "LB_APPROACH", "REPARTITION");
-  Zoltan_Set_Param(zz_, "REMAP", "1");
+  Zoltan_Set_Param(zz_, "REMAP", "0");
   Zoltan_Set_Param(zz_, "IMBALANCE_TOL", "1.05" );
 
   /* Application defined query functions */
@@ -312,4 +502,6 @@ get_edge_list(void *data, int sizeGID, int sizeLID,
     }
   }
 }
-#endif // if HAVE_ZOLTAN
+
+#endif // HAVE_ZOLTAN
+#endif // #ifndef LOADBALNCE_ZOLTAN_HH
