faa8f8ea7dcb2bd4c186ee034201b309c62beb93
dedner <a.s.dedner@warwick.ac.uk>
2014-06-28 20:24:51 +0100

    small fix
    Former-commit-id: c5fe9057cb66e56c626d65cca9f8f31664d01187

--- examples/check-communication.sh
+++ examples/check-communication.sh
@@ -16,15 +16,17 @@ mv main_transport transport_overlapcom
 
 cd ..
 
+echo "non overlapping"
 $MPICALL ./transport_persistent 2 0 3 >& transport_persistent.$1.out
-mv speedup.1 transport_persistent.speedup.$1
+mv speedup.$1 transport_persistent.speedup.$1
 
 cd communication
+echo "overlapping"
 $MPICALL ./transport_overlapcom 2 0 3 >& transport_overlapcom.$1.out
-mv speedup.1 transport_overlapcom.speedup.$1
+mv speedup.$1 transport_overlapcom.speedup.$1
 
 cd ..
 
-grep "finished" transport_persistent.$1out \
+grep "finished" transport_persistent.$1.out \
                 communication/transport_overlapcom.$1.out 
 
