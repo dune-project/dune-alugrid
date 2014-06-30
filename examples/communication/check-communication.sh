0fd4502d57ffe3d69809c419f0bafd4013b4a820
dedner <a.s.dedner@warwick.ac.uk>
2014-06-30 09:41:57 +0100

    make parameter a variable in the script
    Former-commit-id: 7780b9537c7cd6165530f194ae38c060660549b0

--- examples/communication/check-communication.sh
+++ examples/communication/check-communication.sh
@@ -1,6 +1,9 @@
 #/bin/bash
 
+# use the Transport test case
 PROBLEM="TRANSPORT"
+# parameters: use discontinuous advection problem
+PARAM="2 0 3"
 MPICALL="mpirun -np $1"
 
 echo "running on $1 procs"
@@ -11,9 +14,9 @@ cd communication
 make EXTRAFLAGS="-DNO_OUTPUT -DNON_BLOCKING" PROBLEM="$PROBLEM" clean main
 mv main main_blocking
 
-$MPICALL ./main_nonblocking 2 0 3 >& nonblocking.$1.out
+$MPICALL ./main_nonblocking $PARAM >& nonblocking.$1.out
 mv speedup.$1 main_nonblocking.speedup.$1
-$MPICALL ./main_blocking 2 0 3 >& blocking.$1.out
+$MPICALL ./main_blocking $PARAM >& blocking.$1.out
 mv speedup.$1 main_blocking.speedup.$1
 
 grep "finished" nonblocking.$1.out \
