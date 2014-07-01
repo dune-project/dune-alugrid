c59a324cdf6c83d49918e3b8239910ee3265aeda
dedner <a.s.dedner@warwick.ac.uk>
2014-07-01 10:06:27 +0100

    fix script
    Former-commit-id: 4b51dad4fa8509e2e842d563afe849f123264fe3

--- examples/testEfficiency/check-efficiency.sh
+++ examples/testEfficiency/check-efficiency.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-NODES=2
+NODES=3
 PROCSPERNODE=4
 
 # include a machine file here and other flags
@@ -11,25 +11,25 @@ PROBLEM="-DEULER"
 PARAM="21 0 1"
 EXTRAFLAGS="-DNO_OUTPUT -DCALLBACK_ADAPTATION"
 
-make clean
-make EXTRAFLAGS="$EXTRAFLAGS" PROBLEM="$PROBLEM" main
+make EXTRAFLAGS="$EXTRAFLAGS" PROBLEM="$PROBLEM" clean main
 
 P=1
 while [  $P -le $PROCSPERNODE ]; do
-  echo "running ./main $PARAM" with $P processes on one machine"
+  echo "running ./main $PARAM with $P processes on one machine"
   $MPICALL -np $P ./main "$PARAM" >& main.$P.out
-  echo "# running ./main $PARAM" with $P processes on one machine" >> main.$P.out
+  echo "# running ./main $PARAM with $P processes on one machine" >> main.$P.out
   mv speedup.$P transport_persistent.speedup.$P
-  let P=2*P
+  let "P=2*P"
 done
 
-M=2
-while [  $M -le $NODES ]; do
-  let P=M*$PROCSNODE
-  echo "running ./main $PARAM" with $P processes on $M machine"
+N=2
+while [  $N -le $NODES ]; do
+  let "P=N*PROCSPERNODE"
+  echo "running ./main $PARAM with $P processes on $N machine"
   $MPICALL -np $P ./main "$PARAM" >& main.$P.out
-  echo "# running ./main $PARAM" with $P processes on $M machine" >> main.$P.out
+  echo "# running ./main $PARAM with $P processes on $N machine" >> main.$P.out
   mv speedup.$P transport_persistent.speedup.$P
+  let "N=N+1"
 done
 
 grep "finished" main.*.out 
