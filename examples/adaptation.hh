2eeca9a533963175b0804bf700cce17256e9d6ae
Robert Kloefkorn <robertk@posteo.net>
2014-07-17 14:01:08 +0200

    make sure grid is balanced after initial adaptation.
    Former-commit-id: d3de9876ae6888f80f1c83c1ab3aca49a4b43a99

--- examples/adaptation.hh
+++ examples/adaptation.hh
@@ -62,7 +62,7 @@ public:
   /** \brief constructor
    *  \param grid   the grid to be adapted
    */
-  LeafAdaptation ( Grid &grid, const int balanceStep = 1 )
+  LeafAdaptation ( Grid &grid, const int balanceStep = 1, const int balanceCounter = -1 )
   : grid_( grid ),
 #ifdef USE_VECTOR_FOR_PWF
     // create persistent container for codimension 0
@@ -71,7 +71,7 @@ public:
     solution_( 0 ),
     adaptTimer_(),
     balanceStep_( balanceStep ),
-    balanceCounter_( balanceStep-1 ),
+    balanceCounter_( balanceCounter < 0 ? balanceStep-1 : balanceCounter ),
     adaptTime_( 0.0 ),
     lbTime_( 0.0 ),
     commTime_( 0.0 )
@@ -223,8 +223,6 @@ inline void LeafAdaptation< Grid, Vector >::finalize()
   bool callBalance = ((balanceStep_ > 0) && (++balanceCounter_ % balanceStep_ == 0));
   // make sure everybody is on the same track 
   assert( callBalance == grid_.comm().max( callBalance) );
-  // increase balanceCounter if balancing is enabled 
-  if( balanceStep_ > 0 ) ++balanceCounter_;
 
   if( callBalance ) 
   {
