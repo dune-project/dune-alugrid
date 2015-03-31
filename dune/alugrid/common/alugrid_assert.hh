cb07d4bdb8d8644c8de20fc624460f7e14ddb8ae
Robert K <robertk@posteo.org>
2015-03-31 14:09:32 +0200

    use assert.
    Former-commit-id: 6930a027bc713371b975a2161603eeae7fb5646a

--- dune/alugrid/common/alugrid_assert.hh
+++ dune/alugrid/common/alugrid_assert.hh
@@ -1,7 +1,12 @@
 #ifndef ALUGRID_ASSERT_HH
 #define ALUGRID_ASSERT_HH
 
+#include <cassert>
 #include "duneassert.hh"
+
+#define ALUGRIDDEBUG
+#define alugrid_assert(EX) assert((EX))
+#if 0
 
 // this is only of interest when NDEBUG is not set
 // NOTE: defining NO_ALUGRID_DEBUG will disable all ALUGrid asserts
@@ -18,6 +23,7 @@
 # define alugrid_assert(EX) (static_cast<void>(0)) // ((void)sizeof(EX))
 #else
 # define alugrid_assert(EX) dune_assert(EX)
+#endif
 #endif
 
 #endif // ALUGRID_ASSERT_HH
