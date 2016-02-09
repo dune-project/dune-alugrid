c527b2e63eba29872ff09b2ea54fb9690187aec5
Robert K <robertk@posteo.org>
2016-02-09 11:34:36 +0100

    make compile with MPI is not present.
    Former-commit-id: 738e40aa4863111288d31f1d925dd340bd51ea0d

--- examples/diagnostics.hh
+++ examples/diagnostics.hh
@@ -247,9 +247,11 @@ namespace Dune {
             file << "# Procs = " << comm_.size() << " * " << maxThreads << " (MPI * threads)" << std::endl ;
             file << "# Timesteps = " << timesteps_ << std::endl ;
             file << "# Max DoFs (per element): " << maxDofs << std::endl;
+#if HAVE_MPI
 #ifdef ALUGRID_COUNT_GLOBALCOMM
             file << "# allgather allreduce (counters for global communication)" << std::endl;
             file << std::setw( 10 ) << ALU3DSPACE allgatherCalled() << "    " << ALU3DSPACE allreduceCalled() << std::endl;
+#endif
 #endif
             file << "# Elements / timestep: sum    max    min    average  " << std::endl;
             file << avgTimes[ size-1 ] << "  " << maxTimes[ size-1 ] << "  " << minTimes[ size-1 ] << "  " << ((size_t)averageElements) << std::endl;
