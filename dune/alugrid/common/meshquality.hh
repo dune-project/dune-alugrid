76e39f34773e9ed0f193ab0ca5afdcb3c610bf4a
alkaemper <alkaemper@mathematik.uni-stuttgart.de>
2017-08-21 13:25:20 +0200

    add measures for all possible codim combinations and rescale them such that in the regular case the measure is 1
    Former-commit-id: 95622c1d972edbf83ed14fcf0665ba67f90da35c

--- dune/alugrid/common/meshquality.hh
+++ dune/alugrid/common/meshquality.hh
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 #include <dune/grid/common/rangegenerators.hh>
 #include <dune/geometry/referenceelements.hh>
@@ -23,6 +24,29 @@ namespace Dune {
     double minVolLongestEdgeRatio = 1e308;
     double maxVolLongestEdgeRatio = 0;
     double avgVolLongestEdgeRatio = 0;
+
+    double minVolSmallestFaceRatio = 1e308;
+    double maxVolSmallestFaceRatio = 0;
+    double avgVolSmallestFaceRatio = 0;
+
+    double minVolBiggestFaceRatio = 1e308;
+    double maxVolBiggestFaceRatio = 0;
+    double avgVolBiggestFaceRatio = 0;
+
+    double minAreaShortestEdgeRatio = 1e308;
+    double maxAreaShortestEdgeRatio = 0;
+    double avgAreaShortestEdgeRatio = 0;
+
+    double minAreaLongestEdgeRatio = 1e308;
+    double maxAreaLongestEdgeRatio = 0;
+    double avgAreaLongestEdgeRatio = 0;
+
+    //in a regular tetrahedron, we have
+    // volume = (sqrt(2)/12)*edge^3
+    // faceArea = (sqrt(3)/4) *edge^2
+    static const double factorEdge = std::sqrt(2.) / double(12);
+    static const double factorFaceEdge =  std::sqrt( 3. ) / 4.;
+    static const double factorFace = factorEdge * std::pow( ( 1. / factorFaceEdge ), 1.5 )  ;
 
     size_t nElements = 0;
     for( const auto& element : elements( gridView, Dune::Partitions::interiorBorder ) )
@@ -44,6 +68,58 @@ namespace Dune {
         longestEdge  = std::max( longestEdge,  edgeLength );
       }
 
+      double smallestFace = 1e308 ;
+      double biggestFace = 0;
+
+      const int faces = element.subEntities( dim-2 );
+      for( int f = 0 ; f < faces ; ++ f )
+      {
+        const auto& face = element.template subEntity<dim-2>( f );
+        const auto& geo  = face.geometry();
+        assert( geo.corners() == 3 );
+        //( geo.corner( 1 ) - geo.corner( 0 ) ).two_norm();
+        double faceSize = geo.volume();
+        smallestFace = std::min( smallestFace, faceSize );
+        biggestFace  = std::max( biggestFace,  faceSize );
+
+        double shortestFaceEdge = 1e308 ;
+        double longestFaceEdge = 0;
+
+        const int faceEdges = face.subEntities( dim-1 );
+        for( int e = 0 ; e < faceEdges ; ++ e )
+        {
+          const int fe = Dune::ReferenceElements<double, dim>::simplex().subEntity(f,1,e,2);
+          const auto& edge = element.template subEntity<dim-1>( fe );
+          const auto& geo  = edge.geometry();
+          assert( geo.corners() == 2 );
+          //( geo.corner( 1 ) - geo.corner( 0 ) ).two_norm();
+          double edgeLength = geo.volume();
+          shortestFaceEdge = std::min( shortestFaceEdge, edgeLength );
+          longestFaceEdge  = std::max( longestFaceEdge,  edgeLength );
+        }
+
+        //in a regular tetrahedron, we have
+        // volume = (sqrt(2)/12)*edge^3
+        shortestFaceEdge = factorFaceEdge * std::pow( shortestFaceEdge, 2 );
+        longestFaceEdge = factorFaceEdge * std::pow( longestFaceEdge, 2 );
+
+
+        const double areaShortest = faceSize / shortestFaceEdge ;
+        minAreaShortestEdgeRatio  = std::min( minAreaShortestEdgeRatio, areaShortest );
+        maxAreaShortestEdgeRatio  = std::max( maxAreaShortestEdgeRatio, areaShortest );
+        avgAreaShortestEdgeRatio += areaShortest;
+
+        const double areaLongest = faceSize / longestFaceEdge ;
+        minAreaLongestEdgeRatio  = std::min( minAreaLongestEdgeRatio, areaLongest );
+        maxAreaLongestEdgeRatio = std::max( maxAreaLongestEdgeRatio, areaLongest );
+        avgAreaLongestEdgeRatio += areaLongest;
+      }
+
+      //in a regular tetrahedron, we have
+      // volume = (sqrt(2)/12)*edge^3
+      shortestEdge = factorEdge * std::pow( shortestEdge, 3 );
+      longestEdge = factorEdge * std::pow( longestEdge, 3 );
+
       const double volShortest = vol / shortestEdge;
       minVolShortestEdgeRatio  = std::min( minVolShortestEdgeRatio, volShortest );
       maxVolShortestEdgeRatio  = std::max( maxVolShortestEdgeRatio, volShortest );
@@ -53,6 +129,22 @@ namespace Dune {
       minVolLongestEdgeRatio = std::min( minVolLongestEdgeRatio, volLongest );
       maxVolLongestEdgeRatio = std::max( maxVolLongestEdgeRatio, volLongest );
       avgVolLongestEdgeRatio += volLongest;
+
+      //in a regular tetrahedron, we have
+      // volume = (sqrt(2)/12)*edge^3
+      // faceArea = (sqrt(3)/4) *edge^2
+      smallestFace = factorFace * std::pow( smallestFace, 1.5 );
+      biggestFace = factorFace * std::pow( biggestFace, 1.5 );
+
+      const double volSmallest = vol / smallestFace;
+      minVolSmallestFaceRatio = std::min( minVolSmallestFaceRatio, volSmallest );
+      maxVolSmallestFaceRatio  = std::max( maxVolSmallestFaceRatio, volSmallest );
+      avgVolSmallestFaceRatio += volSmallest;
+
+      const double volBiggest = vol / biggestFace ;
+      minVolBiggestFaceRatio = std::min( minVolBiggestFaceRatio, volBiggest );
+      maxVolBiggestFaceRatio = std::max( maxVolBiggestFaceRatio, volBiggest );
+      avgVolBiggestFaceRatio += volBiggest;
 
       ++ nElements;
     }
@@ -70,10 +162,36 @@ namespace Dune {
     avgVolShortestEdgeRatio /= double(nElements);
     avgVolLongestEdgeRatio /= double(nElements);
 
+    minVolSmallestFaceRatio = gridView.grid().comm().min( minVolSmallestFaceRatio );
+    maxVolSmallestFaceRatio = gridView.grid().comm().max( maxVolSmallestFaceRatio );
+    avgVolSmallestFaceRatio = gridView.grid().comm().sum( avgVolSmallestFaceRatio );
+
+    minVolBiggestFaceRatio = gridView.grid().comm().min( minVolBiggestFaceRatio );
+    maxVolBiggestFaceRatio = gridView.grid().comm().max( maxVolBiggestFaceRatio );
+    avgVolBiggestFaceRatio = gridView.grid().comm().sum( avgVolBiggestFaceRatio );
+
+    avgVolSmallestFaceRatio /= double(nElements);
+    avgVolBiggestFaceRatio /= double(nElements);
+
+    minAreaShortestEdgeRatio = gridView.grid().comm().min( minAreaShortestEdgeRatio );
+    maxAreaShortestEdgeRatio = gridView.grid().comm().max( maxAreaShortestEdgeRatio );
+    avgAreaShortestEdgeRatio = gridView.grid().comm().sum( avgAreaShortestEdgeRatio );
+
+    minAreaLongestEdgeRatio = gridView.grid().comm().min( minAreaLongestEdgeRatio );
+    maxAreaLongestEdgeRatio = gridView.grid().comm().max( maxAreaLongestEdgeRatio );
+    avgAreaLongestEdgeRatio = gridView.grid().comm().sum( avgAreaLongestEdgeRatio );
+
+    avgAreaShortestEdgeRatio /= double(4 * nElements);
+    avgAreaLongestEdgeRatio /= double(4 * nElements);
+
     if( gridView.grid().comm().rank() == 0 )
     {
-      std::cout << "MeshQuality: Volume / Longest   " << minVolLongestEdgeRatio << " " << maxVolLongestEdgeRatio << " " << avgVolLongestEdgeRatio << std::endl;
-      std::cout << "MeshQuality: Volume / Shortest  " << minVolShortestEdgeRatio << " " << maxVolShortestEdgeRatio << " " << avgVolShortestEdgeRatio << std::endl;
+      std::cout << "MeshQuality: Volume / Longest Edge  " << minVolLongestEdgeRatio << " " << maxVolLongestEdgeRatio << " " << avgVolLongestEdgeRatio << std::endl;
+      std::cout << "MeshQuality: Volume / Shortest Edge " << minVolShortestEdgeRatio << " " << maxVolShortestEdgeRatio << " " << avgVolShortestEdgeRatio << std::endl << std::endl;
+      std::cout << "MeshQuality: Volume / Biggest Face  " << minVolBiggestFaceRatio << " " << maxVolBiggestFaceRatio << " " << avgVolBiggestFaceRatio << std::endl;
+      std::cout << "MeshQuality: Volume / Smallest Face " << minVolSmallestFaceRatio << " " << maxVolSmallestFaceRatio << " " << avgVolSmallestFaceRatio << std::endl << std::endl;
+      std::cout << "MeshQuality: Area / Longest Edge  " << minAreaLongestEdgeRatio << " " << maxAreaLongestEdgeRatio << " " << avgAreaLongestEdgeRatio << std::endl;
+      std::cout << "MeshQuality: Area / Shortest Edge " << minAreaShortestEdgeRatio << " " << maxAreaShortestEdgeRatio << " " << avgAreaShortestEdgeRatio << std::endl << std::endl;
     }
   }
 
