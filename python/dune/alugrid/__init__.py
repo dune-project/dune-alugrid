e9da4bd5bd7381640b3d7e3527f3ab3be4a0d5a8
dedner <a.s.dedner@warwick.ac.uk>
2017-09-25 18:32:35 +0100

    improved the runtime configuration check for the python package
    Former-commit-id: e7236d8a6caf0233ab74ca364e0f6ab94ac924bb

--- python/dune/alugrid/__init__.py
+++ python/dune/alugrid/__init__.py
@@ -1,9 +1,4 @@
 from ._grids import *
 
 registry = dict()
-registry["grid"] = {
-        "ALU"        : aluGrid,
-        "ALUConform" : aluConformGrid,
-        "ALUCube" :    aluCubeGrid,
-        "ALUSimplex" : aluSimplexGrid,
-    }
+registry["grid"] = grid_registry
