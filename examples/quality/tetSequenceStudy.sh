70247a0d2174c60a3c4974f6bb7f4f06a4a9fa0f
alkaemper <alkaemper@mathematik.uni-stuttgart.de>
2017-10-03 19:02:17 +0200

    unify outputorder of variant threshold and announced
    Former-commit-id: 196fcd651faed98b342ce8b461470a4275585659

--- examples/quality/tetSequenceStudy.sh
+++ examples/quality/tetSequenceStudy.sh
@@ -11,10 +11,10 @@ mkdir meshquality
 for MESH in 00 0 1 2 3 4 5 6 ; do
   mkdir meshquality/tet.${MESH}.msh
   for ANNOUNCED in 0 1 ; do
-    ./main_quality ../dgf/tet.${MESH}.msh.dgf 0 0 ${ANNOUNCED} &> ./meshquality/tet.${MESH}.msh/quality-${ANNOUNCED}-0-0.out
+    ./main_quality ../dgf/tet.${MESH}.msh.dgf 0 0 ${ANNOUNCED} &> ./meshquality/tet.${MESH}.msh/quality-0-0-${ANNOUNCED}.out
     for VARIANT in 1 2 ; do
       for THRESHOLD in `seq 0 35` ; do
-          ./main_quality ../dgf/tet.${MESH}.msh.dgf ${VARIANT} ${THRESHOLD} ${ANNOUNCED} &> ./meshquality/tet.${MESH}.msh/quality-${ANNOUNCED}-${VARIANT}-${THRESHOLD}.out
+          ./main_quality ../dgf/tet.${MESH}.msh.dgf ${VARIANT} ${THRESHOLD} ${ANNOUNCED} &> ./meshquality/tet.${MESH}.msh/quality-${VARIANT}-${THRESHOLD}-${ANNOUNCED}.out
       done
     done
   done
