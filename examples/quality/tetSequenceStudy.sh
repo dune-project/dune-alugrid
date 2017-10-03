8e487095f9964131c2e416cce8bdbb2514109021
alkaemper <alkaemper@mathematik.uni-stuttgart.de>
2017-10-03 18:57:31 +0200

    add a small script to make the tet parameter study
    Former-commit-id: 488d023dd72ec106ee0948be0b01140f61372842

---
+++ examples/quality/tetSequenceStudy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BUILDDIR=${HOME}/software/dune/dune-alugrid/build-optim/examples/quality
+
+cd ${BUILDDIR}
+
+make -j4 main_quality
+
+mkdir meshquality
+
+for MESH in 00 0 1 2 3 4 5 6 ; do
+  mkdir meshquality/tet.${MESH}.msh
+  for ANNOUNCED in 0 1 ; do
+    ./main_quality ../dgf/tet.${MESH}.msh.dgf 0 0 ${ANNOUNCED} &> ./meshquality/tet.${MESH}.msh/quality-${ANNOUNCED}-0-0.out
+    for VARIANT in 1 2 ; do
+      for THRESHOLD in `seq 0 35` ; do
+          ./main_quality ../dgf/tet.${MESH}.msh.dgf ${VARIANT} ${THRESHOLD} ${ANNOUNCED} &> ./meshquality/tet.${MESH}.msh/quality-${ANNOUNCED}-${VARIANT}-${THRESHOLD}.out
+      done
+    done
+  done
+done
