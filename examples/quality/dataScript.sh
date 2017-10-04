63a891d03587b25987ca997aab538d4e1f7a658a
alkaemper <alkaemper@mathematik.uni-stuttgart.de>
2017-10-04 16:36:09 +0200

    adjust datascript to also copy into paper folder
    Former-commit-id: 3c07350cf68fa03dc9c965ba3b06996aec8cd84e

--- examples/quality/dataScript.sh
+++ examples/quality/dataScript.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-BUILDDIR=${HOME}/software/dune/dune-alugrid/build-optim/examples/quality
-
+BUILDDIR=${HOME}/software/dune/dune-alugrid/build-optim/examples/quality/
+PAPERDIR=${HOME}/NMH/git/WeakCompCond/plotdata/
 cd ${BUILDDIR}
 
 for MESH in 00 0 1 2 3 4 5 6 ; do
+  mkdir ${PAPERDIR}/tet.${MESH}.msh
   for ANNOUNCED in 0 1 ; do
     for VARIANT in 1 2 ; do
       cp ./meshquality/tet.${MESH}.msh/${VARIANT}-0-${ANNOUNCED}-meshquality.gnu ./meshquality/tet.${MESH}.msh/threshold-${VARIANT}-${ANNOUNCED}.plot
@@ -15,6 +16,9 @@ for MESH in 00 0 1 2 3 4 5 6 ; do
         grep --after-context=1 "#V0" ./meshquality/tet.${MESH}.msh/quality-${VARIANT}-${THRESHOLD}-${ANNOUNCED}.out | tail -n 1 >> ./meshquality/tet.${MESH}.msh/V0V1-${VARIANT}-${ANNOUNCED}.plot
         grep --after-context=1 "MacroFaces" ./meshquality/tet.${MESH}.msh/quality-${VARIANT}-${THRESHOLD}-${ANNOUNCED}.out | tail -n 1 >> ./meshquality/tet.${MESH}.msh/NonCompat-${VARIANT}-${ANNOUNCED}.plot
       done
+      cp ./meshquality/tet.${MESH}.msh/V0V1-${VARIANT}-${ANNOUNCED}.plot ${PAPERDIR}/tet.${MESH}.msh/
+      cp ./meshquality/tet.${MESH}.msh/threshold-${VARIANT}-${ANNOUNCED}.plot ${PAPERDIR}/tet.${MESH}.msh/
+      cp ./meshquality/tet.${MESH}.msh/NonCompat-${VARIANT}-${ANNOUNCED}.plot ${PAPERDIR}/tet.${MESH}.msh/
     done
   done
 done
