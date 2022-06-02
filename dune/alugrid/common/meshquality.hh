f759c6e95023a7a283ac1605cba290fa6632b4ef
Robert K <robertk@posteo.org>
2022-06-02 14:10:01 +0200

    [bugfix] Only warning in the first call and then just skip in further calls.
    Former-commit-id: 3daa1f1adb5d9515ec7af4bbee0833d8f4bd812e

--- dune/alugrid/common/meshquality.hh
+++ dune/alugrid/common/meshquality.hh
@@ -70,12 +70,12 @@ namespace Dune {
       const double vol = element.geometry().volume();
       const Dune::GeometryType geomType = element.type();
 
-      static bool firstCall = true;
-      if( ! geomType.isSimplex() && firstCall )
+      if( ! geomType.isSimplex() )
       {
-        firstCall = false;
-        if( gridView.comm().rank() == 0 )
+        static bool firstCall = true;
+        if( gridView.comm().rank() == 0 && firstCall )
         {
+          firstCall = false;
           std::cout << "MeshQuality check only works for simplex grids, skipping check!" << std::endl;
         }
         return ;
