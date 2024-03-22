78745326d4f96472797d3fd03c9968c2b9967d8f
Robert K <robertk@posteo.org>
2024-03-22 16:46:10 +0100

    [cleanup][Python] Reduce source files for precompiled modules.
    Former-commit-id: a84d9ae19191810dc081fef44e1c209bdde0df43

--- python/dune/alugrid/registeralugrid_22_simplex.cc
+++
@@ -1,4 +0,0 @@
-#include <config.h>
-#define INCLUDE_REGALUGRID_INLINE
-#include "registeralugrid.hh"
-template void registerALUGrid<2, 2, Dune::simplex>(pybind11::module);
--- python/dune/alugrid/registeralugrid_33_simplex.cc
+++
@@ -1,4 +0,0 @@
-#include <config.h>
-#define INCLUDE_REGALUGRID_INLINE
-#include "registeralugrid.hh"
-template void registerALUGrid<3, 3, Dune::simplex>(pybind11::module);
