a6d619b4449ebad8ce956afe11cd5e5454a31890
Robert K <robertk@posteo.org>
2026-05-05 14:07:45 +0200

    [feature][Python] Add references for ALUGrid.
    Former-commit-id: 3f2a6dcfb2cf50b51d22bf07d437b43bc3c07033

--- python/dune/alugrid/__init__.py
+++ python/dune/alugrid/__init__.py
@@ -3,3 +3,36 @@ from ._grids import *
 
 registry = dict()
 registry["grid"] = grid_registry
+
+# references for ALUGrid
+__cite_dune_module_as__ = """
+@article{alugrid:16,
+  author = {Alk{\\"a}mper, M. and Dedner, A. and Kl{\\"o}fkorn, R. and Nolte, M.},
+  title = {{The DUNE-ALUGrid Module.}},
+  journal = {Archive of Numerical Software},
+  volume = {4},
+  number = {1},
+  year = {2016},
+  pages = {1--28},
+  doi = {10.11588/ans.2016.1.23252}
+}
+"""
+__conforming_citations__ = None
+
+# references for conforming ALUGrid
+def _add_conforming_citation():
+    global __cite_dune_module_as__
+    global __conforming_citations__
+    if __conforming_citations__ is None:
+        __conforming_citations__ = True
+        __cite_dune_module_as__ += """
+@article{dnvb:16,
+  author    = {Alk{\\"{a}}mper, M. and  Kl{\\"o}fkorn, R.},
+  title = {{Distributed Newest Vertex Bisection}},
+  journal = {Journal of Parallel and Distributed Computing},
+  volume = {104},
+  pages = {1 - 11},
+  year = {2017},
+  doi = {10.1016/j.jpdc.2016.12.003}
+}
+"""
